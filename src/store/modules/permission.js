e558969258c765cd365708f1cdef529c7d4e6c8b
CGY-cgy <2783161811@qq.com>
2026-01-26 09:21:52 +0800

    初始化前端代码

---
+++ src/store/modules/permission.js
@@ -0,0 +1,149 @@
+import auth from '@/plugins/auth'
+import router, { constantRoutes, dynamicRoutes } from '@/router'
+import { getRouters } from '@/api/menu'
+import Layout from '@/layout/index'
+import ParentView from '@/components/ParentView'
+import InnerLink from '@/layout/components/InnerLink'
+
+// 匹配views里面所有的.vue文件
+const modules = import.meta.glob('./../../views/**/*.vue')
+
+const usePermissionStore = defineStore(
+  'permission',
+  {
+    state: () => ({
+      routes: [],
+      addRoutes: [],
+      defaultRoutes: [],
+      topbarRouters: [],
+      sidebarRouters: []
+    }),
+    actions: {
+      setRoutes(routes) {
+        this.addRoutes = routes
+        this.routes = constantRoutes.concat(routes)
+      },
+      setDefaultRoutes(routes) {
+        this.defaultRoutes = constantRoutes.concat(routes)
+      },
+      setTopbarRoutes(routes) {
+        this.topbarRouters = routes
+      },
+      setSidebarRouters(routes) {
+        this.sidebarRouters = routes
+      },
+      generateRoutes(roles) {
+        return new Promise(resolve => {
+          // 向后端请求路由数据
+          getRouters().then(res => {
+            const sdata = JSON.parse(JSON.stringify(res.data))
+            const rdata = JSON.parse(JSON.stringify(res.data))
+            const defaultData = JSON.parse(JSON.stringify(res.data))
+            const sidebarRoutes = filterAsyncRouter(sdata)
+            const rewriteRoutes = filterAsyncRouter(rdata, false, true)
+            const defaultRoutes = filterAsyncRouter(defaultData)
+            const asyncRoutes = filterDynamicRoutes(dynamicRoutes)
+            asyncRoutes.forEach(route => {
+              router.addRoute(route)
+            })
+            // 合并所有路由，包括动态路由和后端返回的路由
+            const allRoutes = [...rewriteRoutes, ...asyncRoutes]
+            this.setRoutes(allRoutes)
+            // 只显示灵境管理相关的菜单，过滤掉若依默认菜单
+            const lingjingRoutes = asyncRoutes.filter(route => route.path === '/lingjing')
+            this.setSidebarRouters(constantRoutes.concat(lingjingRoutes))
+            this.setDefaultRoutes(lingjingRoutes)
+            this.setTopbarRoutes(lingjingRoutes)
+            resolve(allRoutes)
+          })
+        })
+      }
+    }
+  })
+
+// 遍历后台传来的路由字符串，转换为组件对象
+function filterAsyncRouter(asyncRouterMap, lastRouter = false, type = false) {
+  // 要隐藏的菜单名称列表
+  const hiddenMenus = ['系统管理', '系统监控', '系统工具', '若依官网', '首页']
+  
+  return asyncRouterMap.filter(route => {
+    // 过滤掉不需要显示的菜单（检查不同字段名，因为后端返回的数据结构可能不同）
+    const menuName = route.menuName || route.title || route.menu_name
+    if (hiddenMenus.includes(menuName)) {
+      return false
+    }
+    
+    if (type && route.children) {
+      route.children = filterChildren(route.children)
+    }
+    if (route.component) {
+      // Layout ParentView 组件特殊处理
+      if (route.component === 'Layout') {
+        route.component = Layout
+      } else if (route.component === 'ParentView') {
+        route.component = ParentView
+      } else if (route.component === 'InnerLink') {
+        route.component = InnerLink
+      } else {
+        route.component = loadView(route.component)
+      }
+    }
+    if (route.children != null && route.children && route.children.length) {
+      route.children = filterAsyncRouter(route.children, route, type)
+      // 如果子菜单都被过滤掉了，那么父菜单也不显示
+      if (route.children.length === 0) {
+        return false
+      }
+    } else {
+      delete route['children']
+      delete route['redirect']
+    }
+    return true
+  })
+}
+
+function filterChildren(childrenMap, lastRouter = false) {
+  var children = []
+  childrenMap.forEach(el => {
+    el.path = lastRouter ? lastRouter.path + '/' + el.path : el.path
+    if (el.children && el.children.length && el.component === 'ParentView') {
+      children = children.concat(filterChildren(el.children, el))
+    } else {
+      children.push(el)
+    }
+  })
+  return children
+}
+
+// 动态路由遍历，验证是否具备权限
+export function filterDynamicRoutes(routes) {
+  const res = []
+  routes.forEach(route => {
+    if (route.permissions) {
+      if (auth.hasPermiOr(route.permissions)) {
+        res.push(route)
+      }
+    } else if (route.roles) {
+      if (auth.hasRoleOr(route.roles)) {
+        res.push(route)
+      }
+    } else {
+      // 如果没有权限要求，直接添加路由
+      res.push(route)
+    }
+  })
+  return res
+}
+
+export const loadView = (view) => {
+  let res
+  for (const path in modules) {
+    const dir = path.split('views/')[1].split('.vue')[0]
+    if (dir === view) {
+      res = () => modules[path]()
+    }
+  }
+  return res
+}
+
+export default usePermissionStore
